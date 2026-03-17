d73914dc76105b808f88c712f853da220c8267bd
Pradeep <pjdevops2022@gmail.com>
2026-03-17 15:33:11 +0530

    Add sample Python app and unit tests

---
+++ src/app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Add 5 + 3 =", add(5,3))
+    print("Multiply 5 * 3 =", multiply(5,3))
